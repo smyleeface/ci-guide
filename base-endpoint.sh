23a89f77ae68ca2c5a801fb17a4301b278dc7c42
smyleeface <patty.ramert@gmail.com>
2017-10-26 07:53:50 -0700

    small mods

--- base-endpoint.sh
+++ base-endpoint.sh
@@ -6,13 +6,14 @@ ACTION=$1
 function deploy_docker_image() {
 	DOCKER_REPO=952671759649.dkr.ecr.us-west-2.amazonaws.com
 	IMAGE_NAME=demo2
-
+	REGION=us-west-2
 	# jet --help
 	# jet run build-application-container
 	# ls -la /project
+	eval $(aws ecr get-login --region ${REGION})
 	docker build /project --file /project/Dockerfile-application -t ${IMAGE_NAME}
 	docker tag ${IMAGE_NAME} ${DOCKER_REPO}/${IMAGE_NAME}:first
-	# docker push ${DOCKER_REPO}/${IMAGE_NAME}:first
+	docker push ${DOCKER_REPO}/${IMAGE_NAME}:first
 }
 
 case "$@" in
